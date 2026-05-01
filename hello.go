8301f1e3c79d868dd5d7680255a1835eeabc6fc7
Leon <infoleonalfrancisco@gmail.com>
2026-05-01 16:41:43 -0600

    Refactor main function to include first and last name variables for personalized greeting

--- hello.go
+++ hello.go
@@ -9,5 +9,14 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Hello())
+
+
+	var firstName,lastName string 
+
+	firstName = "Leon"
+	lastName = "Jose"
+
+	fmt.Printf("My name is %s %s\n", firstName, lastName)
 }
+
 
